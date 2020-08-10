8725ef92fcfa0d3e8c416f0185607628c205b8dc
unknown <himanshugharat44@gmail.com>
2020-08-10 08:12:06 -0500

    ADDED:now we add multiple contacts using collection

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class AddressBookMain {
@@ -12,77 +14,93 @@ public class AddressBookMain {
         int opreator;
         int count = 0;
         Scanner sc = new Scanner(System.in);
-        Person addArray[] = new Person[10];
+        LinkedList<Person> list=new LinkedList();
+        //Person addArray[] = new Person[10];
         while (true) {
-            System.out.println("operations are \n1.add address \n2.update address");
+            System.out.println("operations are \n1.add address \n2.update address \n3.delete");
             System.out.println("enter your choice");
             opreator = sc.nextInt();
             switch (opreator) {
                 case 1:
                     System.out.println("lets add new address entry");
-                    System.out.println("enter the first name");
-                    firstName = sc.next();
-                    System.out.println("enter the last name");
-                    lastName = sc.next();
-                    System.out.println("enter the one word address location ");
-                    address = sc.next();
-                    System.out.println("enter the city name");
-                    city = sc.next();
-                    System.out.println("enter the State");
-                    state = sc.next();
-                    System.out.println("enter the zip code");
-                    zip = sc.nextInt();
-                    System.out.println("enter the phone number");
-                    phoneNumber = sc.nextLong();
-                    addArray[count] = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
-                    System.out.println("succesfully added");
-                    System.out.println(addArray[count]);
-                    count++;
+                    System.out.println("enter the number of entry");
+                    int num=sc.nextByte();
+                    for (int i=0;i<num;i++) {
+                        System.out.println("enter the first name");
+                        firstName = sc.next();
+                        System.out.println("enter the last name");
+                        lastName = sc.next();
+                        System.out.println("enter the one word address location ");
+                        address = sc.next();
+                        System.out.println("enter the city name");
+                        city = sc.next();
+                        System.out.println("enter the State");
+                        state = sc.next();
+                        System.out.println("enter the zip code");
+                        zip = sc.nextInt();
+                        System.out.println("enter the phone number");
+                        phoneNumber = sc.nextLong();
+                        list.add(new Person(firstName, lastName, address, city, state, zip, phoneNumber));
+                        System.out.println("succesfully added");
+                        System.out.println(list.get(count));
+                        count++;
+                    }
+
                     break;
+
+
                 case 2:
-                    System.out.println("update book");
+                    System.out.println("update address book");
                     System.out.println("enter the name");
                     firstName = sc.next();
-                    for (int i = 0; i < count; i++) {
-                        if (addArray[i] != null && addArray[i].getFirstName().equals(firstName)) {
+                    Iterator<Person> i=list.listIterator();
+                    while(i.hasNext()){
+                        int place=0;
+                        int pass=0;
+
+                        if(i.next().getFirstName().matches(firstName)) {
+                            pass = 1;
+                            list.remove(place);
+                        }
+                        if (pass ==1){
                             System.out.println("enter the first name");
                             firstName = sc.next();
-                            addArray[i].setFirstName(firstName);
                             System.out.println("enter the last name");
                             lastName = sc.next();
-                            addArray[i].setLastName(lastName);
                             System.out.println("enter the one word address location ");
                             address = sc.next();
-                            addArray[i].setAddress(address);
                             System.out.println("enter the city name");
                             city = sc.next();
-                            addArray[i].setCity(city);
                             System.out.println("enter the State");
                             state = sc.next();
-                            addArray[i].setState(state);
                             System.out.println("enter the zip code");
                             zip = sc.nextInt();
-                            addArray[i].setZip(zip);
                             System.out.println("enter the phone number");
                             phoneNumber = sc.nextLong();
-                            addArray[i].setPhoneNumber(phoneNumber);
-                            System.out.println(addArray[i]);
+                            list.add(place,new Person(firstName, lastName, address, city, state, zip, phoneNumber));
+                            //System.out.println(i.next());
+                            System.out.println(list);
                         }
+                        place++;
                     }
                     System.out.println("successfully updated");
+                    System.out.println(list);
+                    break;
                 case 3:
                     System.out.println("delete contact");
                     System.out.println("enter the name to delete");
                     firstName=sc.next();
-                    for (int i=0;i<count;i++){
-                        if(addArray[i]!=null&&addArray[i].getFirstName().equals(firstName)){
-                            addArray[i]=null;
-
+                    Iterator<Person> j=list.listIterator();
+                    while(j.hasNext()) {
+                        int place = 0;
+                        if (j.next().getFirstName().matches(firstName)) {
+                            list.remove(place);
                         }
+                        place++;
                     }
                     System.out.println("successfully deleted");
-
             }
+            System.out.println(list);
         }
     }
 }
